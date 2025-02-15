77be3f8667b0e0469e18bcab9875908b765bf88f
Pesso <prashant9193034797@gmail.com>
2025-02-15 21:50:05 +0530

    New Programs added

---
+++ ConvertByteStream.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+public class ConvertByteStream{
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("R.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            BufferedReader br= new BufferedReader(inputStreamReader);
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
